1c85e8bb7e788e97af32143af00f783f6be5e625
sandeepdhml3 <133658133+sandeepdhml3@users.noreply.github.com>
2024-04-28 16:08:30 +0545

    Update AboutCard.js

--- src/components/About/AboutCard.js
+++ src/components/About/AboutCard.js
@@ -8,19 +8,21 @@ function AboutCard() {
       <Card.Body>
         <blockquote className="blockquote mb-0">
           <p style={{ textAlign: "justify" }}>
-            Hi Everyone, I am <span className="purple">Soumyajit Behera </span>
-            from <span className="purple"> Bhubaneswar, India.</span>
+            Hi Everyone, I am <span className="purple">Sandeep Dhamala </span>
+            from <span className="purple"> Jhapa, Nepal.</span>
             <br />
-            I am currently employed as a software developer at Juspay.
+            I am currently employed as a NOC Engineer at CGNET.
             <br />
-            I have completed Integrated MSc (IMSc) in Maths and Computing at BIT
-            Mesra.
+           With a robust skill set encompassing technical expertise, outstanding customer service, web design, coding, and network operations, I ensure seamless performance and customer satisfaction.
             <br />
             <br />
-            Apart from coding, some other activities that I love to do!
+            Apart from ISP, some other activities that I love to do!
           </p>
           <ul>
             <li className="about-activity">
+              <ImPointRight /> Happy Coding
+            </li>
+              <li className="about-activity">
               <ImPointRight /> Playing Games
             </li>
             <li className="about-activity">
@@ -32,9 +34,9 @@ function AboutCard() {
           </ul>
 
           <p style={{ color: "rgb(155 126 172)" }}>
-            "Strive to build things that make a difference!"{" "}
+            "Strive for excellence without waiting for the fruits of your efforts."{" "}
           </p>
-          <footer className="blockquote-footer">Soumyajit</footer>
+          <footer className="blockquote-footer">Sandeep</footer>
         </blockquote>
       </Card.Body>
     </Card>
